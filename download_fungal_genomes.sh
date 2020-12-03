34027af7175d6cd3949056e32fd7f65859bb78f2
Jearce <jessiearce42@gmail.com>
2020-12-03 13:12:30 -0600

    bash script to download fungal genoms from ena

---
+++ download_fungal_genomes.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+set -u
+set -o pipefail
+
+
+if [ $# -eq 0 ]
+then
+  echo "usage: ./download_fungal_genomes.sh <csv file>"
+  echo " Any failed downloads are logged to fungal.downloads.log file."
+fi
+
+export filename=$1
+export ftp_dir="ftp://ftp.ensemblgenomes.org/pub/fungi/release-49/fasta/fungi_basidiomycota1_collection"
+
+for name in $(python extract_and_format_fungi_names.py ${filename} --strict)
+do
+  echo "------------------------------------"
+  echo "Downloading proteins for ${name}"
+  echo "-------------------------------------"
+  if ! wget "${ftp_dir}/${name}/cds/*.cds.all.fa.gz"
+  then
+    echo "${name} was not downloaded" >> fungal.downloads.log
+  fi
+done
+exit
