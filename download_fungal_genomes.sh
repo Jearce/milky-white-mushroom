8ed01056f547a686c6e3029f8b42e67864bc5753
Jearce <jessiearce42@gmail.com>
2020-12-03 13:34:04 -0600

    Removed extra argument passed to python script

--- download_fungal_genomes.sh
+++ download_fungal_genomes.sh
@@ -8,12 +8,13 @@ if [ $# -eq 0 ]
 then
   echo "usage: ./download_fungal_genomes.sh <csv file>"
   echo " Any failed downloads are logged to fungal.downloads.log file."
+  exit
 fi
 
 export filename=$1
 export ftp_dir="ftp://ftp.ensemblgenomes.org/pub/fungi/release-49/fasta/fungi_basidiomycota1_collection"
 
-for name in $(python extract_and_format_fungi_names.py ${filename} --strict)
+for name in $(extract_and_format_fungi_names.py ${filename} --strict)
 do
   echo "------------------------------------"
   echo "Downloading proteins for ${name}"
