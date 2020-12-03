36be300f63e49fc6ec2d0d57316e10a07bc31d3f
Jearce <jessiearce42@gmail.com>
2020-12-03 13:35:08 -0600

    removed strict funcationality from extract_and_format_fungi_names.py

--- extract_and_format_fungi_names.py
+++ extract_and_format_fungi_names.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import re
 import sys
 
@@ -5,18 +6,11 @@ import pandas
 from Bio import Entrez
 
 csv_path = sys.argv[1]
-strict = None
-if len(sys.argv) == 3: 
-    strict = sys.argv[2]
 
 df = pandas.read_csv(csv_path).dropna(axis=1,how="all")
 try:
     for name in df["Name"]:
         new_name = re.sub(r"[\s-]","_",re.sub("[.()]","",name)).lower()
-        if strict == "--strict":
-            print(new_name)
-        else:
-            genus_species = "_".join(new_name.split("_")[:2])
-            print(genus_species)
+        print(new_name)
 except KeyError as e:
     print(f"Unexpected data format. No column with name: {e}")
