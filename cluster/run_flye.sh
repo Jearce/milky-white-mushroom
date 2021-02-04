e81a235bbbc1eeb145582b38cb52bdb2387def3e
Jearce <jessiearce42@gmail.com>
2021-02-04 14:09:28 -0600

    update to run_flye.sh

--- cluster/run_flye.sh
+++ cluster/run_flye.sh
@@ -2,7 +2,7 @@
 #SBATCH -J flye
 #SBATCH -o flye.o%j
 #SBATCH -c 20
-#SBATCH --mem=18G
+#SBATCH --mem=32G
 #SBATCH -t 15:00:00
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=jearce2@uh.edu
@@ -20,17 +20,19 @@ then
   exit
 fi
 
-conda activate flye-env
-
 export nanoreads=$1
 export r1=$2
 export r2=$3
-export prefix=$4
+export assembly_dir=$4
 
-export assembly_folder=${prefix}_flye
-flye --nano-raw ${nanoreads} --out-dir ${assembly_folder} --threads 20 -i 3
+#assemble genome
+conda activate flye-env
 
-cd ${assembly_folder}
+flye --nano-raw ${nanoreads} --out-dir ${assembly_dir} --threads 20 -i 3
+cd ${assembly_dir}
+
+conda deactivate
+
 
 #set up polishing environment 
 conda activate polish-env
@@ -39,16 +41,14 @@ module load SAMtools/1.9-intel-2017b
 #three rounds of polishing
 assembly="assembly.fasta"
 for i in {1..3}
+do
   polish_dir="polish_round_${i}"
   mkdir polish_folder
-
   aln="${polish_dir}/aln-sorted.bam"
   pilon_out="${polish_dir}/pilon_out"
-  minimap3 -ax -t 18 sr ${assembly} ${r1} ${r2} | samtools view -u | samtools sort -@ 20 > ${aln}
-  pilon --genome ${assembly} --frags ${aln} --outdir ${pilon_out}
+  minimap3 -ax -t 20 sr ${assembly} ${r1} ${r2} | samtools view -u | samtools sort -@ 20 > ${aln}
+  pilon --genome ${assembly} --frags ${aln} --threads 20 --outdir ${pilon_out}
   assembly="${pilon_out}/pilon.fasta"
-
-do
-
+done
 
 conda deactivate
