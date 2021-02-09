ea9ca0d0a4f61e8df03c01151ab261530037dc06
Jearce <jessiearce42@gmail.com>
2021-02-09 15:28:45 -0600

    script to run barrnap

---
+++ run_barrnap.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -J flyeSub
+#SBATCH -o flyeSub.o%j
+#SBATCH -c 10
+#SBATCH --mem=18G
+#SBATCH -t 02:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=jearce2@uh.edu
+set -e
+set -u
+set -o pipefail
+
+if [ $# -ne 1 ]
+then
+  echo "Incorrect number of arguments"
+  echo "run_barrnap.sh <contigs>"
+  exit
+fi
+
+
+#set local anaconda3 env located in project dir
+set +eu
+source ~/.bashrc 
+
+conda activate barrnap
+
+export contigs=$1
+barrnap --threads 10 --evalue 1e-16 -o rrna.fa < ${contigs} > rrna.gff
+
+conda deactivate
+
+
