a88718f20e337fea6c47e62e348e3327d1236b6d
Jearce <jessiearce42@gmail.com>
2021-02-22 13:38:49 -0600

    run trnascan script

---
+++ cluster/run_trnascan.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+#SBATCH -J trna
+#SBATCH -o trna.o%j
+#SBATCH -c 12
+#SBATCH --mem=10G
+#SBATCH -t 02:00:00
+#SBATCH --mail-type=END,FAIL
+
+set -e
+set -u
+set -o pipefail
+
+#set local anaconda3 env located in project dir
+set +eu
+source ~/.bashrc 
+
+
+if [ $# -ne 1 ]
+then
+	echo "Incorrect number of arguments"
+	echo "$0 <contigs>"
+	exit
+fi
+
+export contigs=$1
+
+conda activate trnascan-se-env
+
+tRNAscan-SE -E -o trnas.fasta -f trnas_stuctures.txt -m trna.models -H ${contigs}
+
+conda deactivate
