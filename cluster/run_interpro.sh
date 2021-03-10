7f88899c0c56532b7a6db8a8c92c79719d6e7dd2
Jearce <jessiearce42@gmail.com>
2021-03-10 12:02:19 -0600

    run interpro script

---
+++ cluster/run_interpro.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#SBATCH -J blastp
+#SBATCH -o blastp.o%j
+#SBATCH -c 20
+#SBATCH --mem=18G
+#SBATCH -t 10:00:00
+#SBATCH --mail-type=END,FAIL
+
+set -e
+set -u
+set -o pipefail
+
+#set local anaconda3 env located in project dir
+set +eu
+source ~/.bashrc 
+
+
+if [ $# -ne 1 ]
+then
+  echo "Incorrect number of arguments"
+  echo "${0} <protein fasta file>"
+  exit
+fi
+
+export fasta=$1
+
+interproscan.sh \
+  -d interpro_out\
+  -b mwm \
+  -f tsv, gff3, html, json\
+  -goterms\
+  -cpu 20\
+  -i ${fasta}
+
